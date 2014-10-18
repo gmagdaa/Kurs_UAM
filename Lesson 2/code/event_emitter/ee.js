0d70aac93806ad712dce2445f2b030c67c449e0c
gmagdaa <gmagdaa@op.pl>
2014-10-18 18:46:29 +0200

    Zadanie.

--- Lesson 2/code/event_emitter/ee.js
+++ Lesson 2/code/event_emitter/ee.js
@@ -11,11 +11,25 @@
 	};
 
 	EE.prototype.on = function (eventName, listener, context) {
+		if (!this.listeners[eventName]) {
+			this.listeners[eventName] = [];
+		}
+		var element = {funkcja: listener, kontekst: context};
+		this.listeners[eventName].push(element);
 
 	};
 
 	EE.prototype.emit = function (eventName /*, other args...*/) {
-
+		var args = [];
+		for (var i = 1; i < arguments.length; i++) {
+			args.push(arguments[i]);
+		}
+		if (this.listeners[eventName]) {
+			for (var i=0; i<this.listeners[eventName].length; i++) {
+				this.listeners[eventName][i].funkcja.apply(this.listeners[eventName][i].kontekst, args);
+			}
+		}
+				
 	};
 
 //	var ee = new EE();
