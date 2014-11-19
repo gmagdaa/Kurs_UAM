ac1b6be2c9540653fb1e0a1b79fd76761dfd9efa
gmagdaa <gmagdaa@op.pl>
2014-11-19 08:21:18 +0100

    Zmiany.

--- Zadania/code/code.js
+++ Zadania/code/code.js
@@ -2,6 +2,8 @@ var button;
 var pole;
 var activeElement;
 var tab = [];
+var all = 0;
+var active = 0;
 
 window.onload = function() {
 	button = document.getElementById("button1");
@@ -22,28 +24,42 @@ window.onload = function() {
 		}
 		
 		if (czyIstnieje) {
-			alert("Hello, to już było");
+			alert("Already Exists");
 		} else {
 			text = document.createElement('li');
 			text.textContent = tresc;
 			document.getElementById("ul1").appendChild(text);
 			tab.push(tresc);
+			all = all + 1;
+			var footer = document.getElementById("footer_all");
+			footer.textContent = all;
 		}
-										
-		
-
-
 	});
-
+	
 	
 	document.getElementById("ul1").addEventListener("click", function(event) {
-		if (activeElement)
+		
+		activeElement = event.target;
+		
+
+		if (activeElement.classList.contains("active")) {
+			activeElement.classList.remove("active");
+			active = active - 1;
+			var footer2 = document.getElementById("footer_active");
+			footer2.textContent = active;
+		} else {		
+
+		activeElement.classList.add("active");
+		active = active + 1;
+		var footer2 = document.getElementById("footer_active");
+		footer2.textContent = active;
+		}
+		
+		
+				/* if (activeElement)
 		{
 			activeElement.classList.remove("active");
-		}
-		activeElement = event.target;
-		activeElement.classList.add("active");
+		}  */
 	});
 };
-	
 	
